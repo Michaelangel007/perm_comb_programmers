328431f770fc66ba9fd82ce2c1db8638799cd852
Michaelangel007 <testcraft@sharedcraft.com>
2015-12-28 15:12:51 -0800

    Added note about C11 and C99 VLA clusterfuck

--- comb_fastswap.cpp
+++ comb_fastswap.cpp
@@ -4,28 +4,41 @@ https://github.com/Michaelangel007/perm_comb_programmers
 */
 
 #include <stdio.h>
+#include <alloca.h>
 #include <string.h>
 
 // Combinations
+
     char* itoa_comb_fast( unsigned int n, int base, int length, const char MASTER_SET[] = "0123456789ABCDEF" )
     {
         const  int          MAX_DIGITS = 32;
-        static char output[ MAX_DIGITS + 1 ];
-
-        static char set[ MAX_DIGITS ];
-        memcpy( set, MASTER_SET, base ); // Optimization: strlen( SET )= base
+        static char output[ MAX_DIGITS + 1 ]; // base=2 output in binary is 32 digits
 
         if (base <  2) base =  2;
         if (base > 16) base = 16;
 
-        int r;
+        // "The nice thing about standards is that you have so many to choose from." -- Andrew Tanenbaum
+        // Note: Variable Lengths Arrays ARE supported by C99 but confusingly
+        //       are NOT part of the C++11 Standard (§ 8.3.4/1) nor are they in C14.
+        // See the document: http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2013/n3820.html
+        // If you have a broken compiler *without* C99 support you'll need to use alloca().
+        // If you have a functioning compiler *with* C99 support it is safer to use:
+        //     char set[ base ];
+#if 1
+        char *set = (char*) alloca( base + 1 );
+#else
+        char set[ base ]; // variable length array
+#endif
+        memcpy( set, MASTER_SET, base ); // Optimization: strlen( MASTER_SET ) == base
+
+        int   r; // remainder
         char *dst = output;
 
         do
         {
             r = n % base;
             n = n / base;
-            *dst++ = set[ r ];
+            *dst++ = set[ r      ];
             set[r] = set[ --base ]; 
             // Optimization: instead of shifting workset over by one character
             // We move the last element into the slot that was just used
