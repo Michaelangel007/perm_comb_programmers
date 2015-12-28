3cb6de28128a6a17a224e9a036dcc09d90988aad
Michaelangel007 <testcraft@sharedcraft.com>
2015-12-28 14:39:49 -0800

    Simplify comb_fastswap.cpp

--- comb_fastswap.cpp
+++ comb_fastswap.cpp
@@ -4,38 +4,16 @@ https://github.com/Michaelangel007/perm_comb_programmers
 */
 
 #include <stdio.h>
-#include <stdint.h>
 #include <string.h>
 
-// Utility
-
-    /** Search for key in vals
-     * @param size     The size of the array to search
-     * @param haystack The array (vals) to search
-     * @param needle   They key to find
-     */
-    int find( const size_t size, const char *haystack, const char needle )
-    {
-        const char *src = haystack;
-        const char *end = haystack + size;
-
-        while( src < end )
-        {
-            if( *src == needle )
-                return (src - haystack);
-            src++;
-        }
-        return -1; // not found
-    }
-
 // Combinations
-    char* itoa_comb_fast( uint32_t n, int base, int length, const char SET[] = "0123456789ABCDEF" )
+    char* itoa_comb_fast( unsigned int n, int base, int length, const char MASTER_SET[] = "0123456789ABCDEF" )
     {
-        const  int  MAX_DIGITS = 32;
+        const  int          MAX_DIGITS = 32;
         static char output[ MAX_DIGITS + 1 ];
 
         static char set[ MAX_DIGITS ];
-        memcpy( set, SET, base ); // Optimization: strlen( SET )= base
+        memcpy( set, MASTER_SET, base ); // Optimization: strlen( SET )= base
 
         if (base <  2) base =  2;
         if (base > 16) base = 16;
@@ -58,39 +36,41 @@ https://github.com/Michaelangel007/perm_comb_programmers
     }
 
 // Demo
-    /* Mapping unique id to combination
+    /*
+        Map unique id to combination with fast swap
 
-        Dec  Bin  Elem
-        #0   00   A
-        #1   01   B
-        #2   10   C
-        #3   11   D
+            Dec  Bin  Elem
+            #0   00   A
+            #1   01   B
+            #2   10   C
+            #3   11   D
 
-        Id  set[r]
-        # 0 ADC
-        # 1 BAC
-        # 2 CAD
-        # 3 DAC
-        # 4 ABD
-        # 5 BDA
-        # 6 CBA
-        # 7 DBA
-        # 8 ACD
-        # 9 BCA
-        #10 CDA
-        #11 DCA
-        #12 ADB
-        #13 BAD
-        #14 CAB
-        #15 DAB
-        #16 ABC
-        #17 BDC
-        #18 CBD
-        #19 DBC
-        #20 ACB
-        #21 BCD
-        #22 CDB
-        #23 DCB
+        Ouput:
+            Id  set[r]
+            # 0 ADC
+            # 1 BAC
+            # 2 CAD
+            # 3 DAC
+            # 4 ABD
+            # 5 BDA
+            # 6 CBA
+            # 7 DBA
+            # 8 ACD
+            # 9 BCA
+            #10 CDA
+            #11 DCA
+            #12 ADB
+            #13 BAD
+            #14 CAB
+            #15 DAB
+            #16 ABC
+            #17 BDC
+            #18 CBD
+            #19 DBC
+            #20 ACB
+            #21 BCD
+            #22 CDB
+            #23 DCB
     */
     void demo_comb_fastswap()
     {
@@ -99,13 +79,13 @@ https://github.com/Michaelangel007/perm_comb_programmers
         int  digits = 3;
         char set[]  = "ABCD";
 
-        printf( "= Combinations =\n" );
-        printf( "Id   set[r]\n" );
+        printf( "= Combinations (Fast Swap) =\n" );
+        printf( "Id set[r]\n" );
 
         for( int i = 0; i < 24; i++ )
         {
             output = itoa_comb_fast( i, base, digits, set );
-            printf( "#%2d %s\n", i, expect );
+            printf( "#%2d %s\n", i, output );
         }
 
         printf( "\n" );
