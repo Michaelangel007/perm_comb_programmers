3880901c3eb9f4333d06e3917ae90124dc8e070e
michaelangel007 <testcraft@sharedcraft.com>
2015-12-29 10:33:14 -0800

    WIP split demo into seperate files

---
+++ print_perm_comb.cpp
@@ -0,0 +1,162 @@
+/*
+Understanding Permutations and Combinations for Programmers
+https://github.com/Michaelangel007/perm_comb_programmers
+
+Alt. Title: The secret to understanding Permutations and Combinations for Programmers
+3rd. Title: Programmer's Guide to Combinations and Permutations
+*/
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+// Utility
+    #include "util.h"
+
+// Permutations
+    #include "perm.h"
+
+// Combinations
+    /*
+            // Remove 'r'th element
+            // http://stackoverflow.com/questions/3751797/can-i-call-memcpy-and-memmove-with-number-of-bytes-set-to-zero
+            // C99 standard (7.21.1/2):
+            //    Where an argument declared as size_t n specifies the length of the array for a function, n can have the value zero on a call to that function. Unless explicitly stated otherwise in the description of a particular function in this subclause, pointer arguments on such a call shall still have valid values, as described in 7.1.4. On such a call, a function that locates a character finds no occurrence, a function that compares two character sequences returns zero, and a function that copies characters copies zero characters.
+            //int digits = base - r;
+            //if( digits > 0 )
+            base--; // Combination = decreasing base
+            memcpy( set + r, set + r + 1, base - r ); // Remove set[r] element
+    */
+    #include "comb_shift.h"
+    #include "comb_fastswap.h"
+    #include "comb_almost.h"
+    #include "comb_natural.h"
+
+    /*
+       https://www.mathsisfun.com/combinatorics/combinations-permutations-calculator.html
+
+       | Ordered? | Repetition? | Type                                   |
+       | no       | no          | Combination = n! / ((n-r)! * r!)       |
+       | no       | yes         | Combination = (n+r-1)! / ((n-1)! * r!) |
+       | yes      | no          | Permutation = n! / (n-r)!              |
+       | yes      | yes         | Permutation = n^r                      |
+
+                ( n )
+       P(n,r) = (   ) = 
+                ( r )
+
+                ( n )   n!
+       C(n,r) = (   ) = ---
+                ( r )   r!(n-r!)
+    */
+
+// Demo
+    // Print Permutations & Combinations
+    void permutations()
+    {
+        char *expect, *actual;
+        int  base   = 4;
+        int  digits = 3;
+        char set[]  = "ABCD";
+
+        printf( "= Permutations =\n" );
+       
+        for( int i = 0; i < 64; i++ )
+        {
+            expect = itoa_perm( i, base, digits, set );
+            printf( "#%2d: %s", i, expect );
+
+            int n  = atoi_perm( expect, base, set );
+            actual = itoa_perm( n, base, digits, set );
+
+            printf( " -> %2d -> %s\n", n, actual );
+        }
+
+        printf( "\n" );
+    }
+
+/* Mapping unique id to combination
+
+      Dec  Bin  Elem
+       #0   00   A
+       #1   01   B
+       #2   10   C
+       #3   11   D
+
+    Id    memcpy()   set[r] Different   Natural
+    # 0   ABC        ADC    *20         ABC 0 0 0
+    # 1   BAC        BAC                ABD 0 0 1
+    # 2   CAB        CAD    *14         ACB 0 1 0
+    # 3   DAB        DAC    *15         ACD 0 1 2
+    # 4   ACB        ABD    *12         ADB 0 2 0
+    # 5   BCA        BDA    *9          ADC 0 2 1
+    # 6   CBA        CBA                BAC 1 0 0
+    # 7   DBA        DBA                BAD 1 0 1
+    # 8   ADB        ACD    *16         BCA 1 
+    # 9   BDA        BCA    *5          BCD 1
+    #10   CDA        CDA                BDA 1
+    #11   DCA        DCA                BDC 1
+    #12   ABD        ADB    *8          CAB 2
+    #13   BAD        BAD                CAD 2
+    #14   CAD        CAB    *2          CBA 2
+    #15   DAC        DAB    *3          CBD 2
+    #16   ACD        ABC    *0          CDA 2
+    #17   BCD        BDC    *21         CDB 2
+    #18   CBD        CBD                DAB 3
+    #19   DBC        DBC                DAC 3
+    #20   ADC        ACB    *4          DBA 3
+    #21   BDC        BCD    *17         DBC 3
+    #22   CDB        CDB                DCA 3
+    #23   DCB        DCB                DCB 3 2 1
+*/
+    
+    void combinations()
+    {
+        char *expect, *actual;
+        int  base   = 4;
+        int  digits = 3;
+        char set[]  = "ABCD";
+        int  n;
+
+        printf( "= Combinations =\n" );
+        printf( "Id#   shift: memcpy()    fast: set[r]       Almost             Natural\n" );
+
+        for( int i = 0; i < 24; i++ )
+//int i = 19;
+        {
+            printf( "#%2d", i );
+
+            expect = itoa_comb_shift( i, base, digits, set );
+            printf( "   %s", expect );
+                n  = atoi_comb_shift( expect, base, set );
+            actual = itoa_comb_shift( n, base, digits, set );
+            printf( " -> %2d -> %s", n, actual );
+
+            expect = itoa_comb_fastswap( i, base, digits, set );
+            printf( "   %s", expect );
+                n  = atoi_comb_fastswap( expect, base, set );
+            actual = itoa_comb_fastswap( n, base, digits, set );
+            printf( " -> %2d -> %s", n, actual );
+
+            expect = itoa_comb_almost( i, base, digits, set );
+            printf( "   %s", expect );
+                n  = atoi_comb_almost( expect, base, set );
+            actual = itoa_comb_almost( n, base, digits, set );
+            printf( " -> %2d -> %s", n, actual );
+
+            expect = itoa_comb_natural( i, base, digits, set );
+            printf( "   %s", expect );
+
+            printf( "\n" );
+        }
+
+        printf( "\n" );
+    }
+
+int main()
+{
+    permutations();
+    combinations();
+
+    return 0;
+}
